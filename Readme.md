4e74ec8e1f30fc6e62693e849eabc949a03ea6ea
Vinod Kiran <vinodkiran@usa.net>
2012-06-08 21:47:30 +0530

    First commit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Signature to Image
+
+Signature to Image: A supplemental script for Signature Pad that generates an image of the signature’s JSON output server-side using Java.
+
+Signature Pad: <http://thomasjbradley.ca/lab/signature-pad>
+
+Copyright Vinod Kiran <vinodkiran@usa.net>
+
+## Quick Start
+```java
+public static BufferedImage convertJsonToImage(String jsonString);
+```
+
+## Other Solutions
+<http://thomasjbradley.ca/lab/signature-to-image/#othersolutions>
+
+## Source Code
+<http://github.com/vinodkiran/SignatureToImageJava>
+
+## License
+Signature Pad is licensed under the [New BSD license](https://github.com/vinodkiran/SignatureToImageJava/blob/master/NEW-BSD-LICENSE.txt).
---
+++ src/main/java/signaturepad/SignatureToImage.java
@@ -0,0 +1,33 @@
+package signaturepad;
+
+import com.google.gson.Gson;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ *	Signature to Image: A supplemental script for Signature Pad that
+ *	generates an image of the signature’s JSON output server-side using Java.
+ *
+ *	@project	signaturetoimage
+ *	@author		Vinod Kiran (vinodkiran@usa.net)
+ *	@link		http://github.com/vinodkiran/SignatureToImageJava
+ *	@license	New BSD License
+ *	@version	1.0.0
+ */
+public class SignatureToImage {
+
+    public static BufferedImage convertJsonToImage(String jsonString){
+        Gson gson = new Gson();
+        SignatureLine[] signatureLines = gson.fromJson(jsonString, SignatureLine[].class);
+        BufferedImage offscreenImage = new BufferedImage(200, 50, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = offscreenImage.createGraphics();
+        g2.setColor(Color.white);
+        g2.fillRect(0,0,200,50);
+        g2.setPaint(Color.black);
+        for (SignatureLine line : signatureLines ) {
+            g2.drawLine(line.lx, line.ly, line.mx, line.my);
+        }
+        return offscreenImage;
+    }
+}
